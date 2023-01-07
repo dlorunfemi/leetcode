84136aa1bd5a9736b375b5ca9e37a1bdf40621a0
dlorunfemi <dlorunfemi@gmail.com>
2023-01-07 23:02:57 +0100

    88. Merge Sorted Array soln 1

--- array/easy/35. Search Insert Position.php
+++ array/easy/35. Search Insert Position.php
@@ -1,1 +1,40 @@
 <?php
+
+class Solution
+{
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     *
+     **/
+    function searchInsert($nums, $target) {
+        $low = 0;
+        $high = count($nums) - 1;
+
+        while ($low <= $high) {
+            $mid = floor(($high + $low) / 2);
+
+            if ($target === $nums[$mid]) {
+                return $mid;
+            } elseif ($target > $nums[$mid]) {
+                $low = $mid + 1;
+            } else {
+                $high = $mid - 1;
+            }
+        }
+
+        return $low;
+    }
+
+}
+
+
+$case_1 = new Solution();
+echo 'Test Case: ' . $case_1->searchInsert([1,3,5,6], 5) . '<br />';
+
+$case_2 = new Solution();
+echo 'Test Case: ' . $case_2->searchInsert([1,3,5,6], 2) . '<br />';
+
+$case_3 = new Solution();
+echo 'Test Case: ' . $case_3->searchInsert([1,3,5,6], 7) . '<br />';
--- array/easy/88. Merge Sorted Array.php
+++ array/easy/88. Merge Sorted Array.php
@@ -1,1 +1,20 @@
 <?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums1
+     * @param Integer $m
+     * @param Integer[] $nums2
+     * @param Integer $n
+     * @return NULL
+     */
+    function merge1(&$nums1, $m, $nums2, $n) {
+        $k = $m + $n - 1;
+        $m = $m - 1;
+        $n = $n - 1;
+        while ($n >= 0) {
+            $nums1[$k--] = ($nums1[$m] > $nums2[$n]) ? $nums1[$m--] : $nums2[$n--];
+        }
+    }
+}
