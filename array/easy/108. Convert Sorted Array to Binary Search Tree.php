42de8dece5dbf26c059a3af3dcdbc106da1dc62a
dlorunfemi <dlorunfemi@gmail.com>
2023-01-08 23:56:42 +0100

    108. Convert Sorted Array to Binary Search Tree

--- array/easy/108. Convert Sorted Array to Binary Search Tree.php
+++ array/easy/108. Convert Sorted Array to Binary Search Tree.php
@@ -1,1 +1,34 @@
 <?php
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return TreeNode
+     */
+    function sortedArrayToBST($nums) {
+        if (!$nums) {
+            return null;
+        }
+        $m = floor(count($nums) / 2);
+
+        return new TreeNode(
+            $nums[$m],
+            $this->sortedArrayToBST(array_slice($nums, 0, $m)),
+            $this->sortedArrayToBST(array_slice($nums, $m + 1))
+        );
+    }
+}
